0119e615247ff374d300a2352016bd619bf90464
positoy <positoy@gmail.com>
2020-07-01 09:06:11 +0900

    use DispatcherSevlet to take Controller under /app/*

---
+++ src/main/java/io/github/positoy/HelloController.java
@@ -0,0 +1,16 @@
+package io.github.positoy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String helloApp() {
+        return "hello " + helloService.getName();
+    }
+}
--- src/main/java/io/github/positoy/WebConfig.java
+++ src/main/java/io/github/positoy/WebConfig.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Configuration
-@ComponentScan(useDefaultFilters = true, excludeFilters = @ComponentScan.Filter(Controller.class))
-public class AppConfig {
-    @Autowired
-    HelloService helloService;
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+public class WebConfig {
+
 }
